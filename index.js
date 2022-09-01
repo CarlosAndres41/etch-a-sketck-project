38570c86a5d8d8c099547c1c7fe10b26430a8373
CarlosAndres <solrak41@gmail.com>
2022-08-31 20:26:13 -0500

    Replace hover state by mouseover event listener for inner divs

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ let gridContainer = document.getElementById('grid-container');
 
 for (let i = 0; i < gridSize; i++) {
     let gridDiv = document.createElement('div');
-    gridDiv.classList.add('inner-div')
+    gridDiv.classList.add('inner-div');
+    gridDiv.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'red';
+    })
     gridContainer.appendChild(gridDiv);
 
 }
