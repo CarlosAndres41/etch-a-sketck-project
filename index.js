6f2c74b7320af65eaa81eacfb0064ce7bccd9ac5
CarlosAndres <solrak41@gmail.com>
2022-08-31 21:16:31 -0500

    Add styling to the webpage, increase opacity of the inner div when mouseover

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let gridSize = 16 * 16;
+let numberOfSquares = 50;
+
+let gridSize = numberOfSquares ** 2;
 
 let gridContainer = document.getElementById('grid-container');
 
 for (let i = 0; i < gridSize; i++) {
     let gridDiv = document.createElement('div');
     gridDiv.classList.add('inner-div');
+
+    let opacity = 0.1;
     gridDiv.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = 'red';
+        opacity += 0.1;
+        event.target.style.backgroundColor = 'rgb(199, 251, 43)';
+        if (opacity <= 1) {
+            event.target.style.opacity = opacity;
+        };
     })
     gridContainer.appendChild(gridDiv);
 
